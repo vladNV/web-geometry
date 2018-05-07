29fb7277696d95246a547111df0936ebd2f1b0bc
Vlad <vlad.nagaev.vn@gmail.com>
2018-05-07 20:58:30 +0300

    some adjusments

--- src/main/java/geometry/Application.java
+++ src/main/java/geometry/Application.java
@@ -1,20 +1,18 @@
 package geometry;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
